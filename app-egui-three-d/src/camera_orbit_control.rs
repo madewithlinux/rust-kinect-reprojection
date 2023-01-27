dfec9268c2cdd05eb8e511839a7942ba4f6b2fc0
madewithlinux <6527677+madewithlinux@users.noreply.github.com>
2023-01-27 11:55:51 -0600

    orbit camera

---
+++ app-egui-three-d/src/camera_orbit_control.rs
@@ -0,0 +1,58 @@
+use three_d::*;
+
+/// similar to theee-d OrbitControl, but with customizable speed
+pub struct CameraOrbitControl {
+    control: CameraControl,
+}
+
+impl CameraOrbitControl {
+    /// Creates a new orbit control with the given target and minimum and maximum distance to the target.
+    pub fn new(target: Vec3, min_distance: f32, max_distance: f32) -> Self {
+        Self {
+            control: CameraControl {
+                left_drag_horizontal: CameraAction::OrbitLeft { target, speed: 0.5 },
+                left_drag_vertical: CameraAction::OrbitUp { target, speed: 0.5 },
+                scroll_vertical: CameraAction::Zoom {
+                    min: min_distance,
+                    max: max_distance,
+                    speed: 0.1,
+                    target,
+                },
+                ..Default::default()
+            },
+        }
+    }
+
+    fn set_orbit_speed(action: &mut CameraAction, new_speed: f32) {
+        match action {
+            CameraAction::OrbitUp { speed, .. } | CameraAction::OrbitLeft { speed, .. } => {
+                *speed = new_speed;
+            }
+            _ => (),
+        }
+    }
+    pub fn set_orbit_drag_speed(&mut self, drag_speed: f32) {
+        Self::set_orbit_speed(&mut self.control.left_drag_horizontal, drag_speed);
+        Self::set_orbit_speed(&mut self.control.left_drag_vertical, drag_speed);
+    }
+
+    /// Handles the events. Must be called each frame.
+    pub fn handle_events(&mut self, camera: &mut Camera, events: &mut [Event]) -> bool {
+        if let CameraAction::Zoom {
+            speed,
+            target,
+            min,
+            max,
+        } = &mut self.control.scroll_vertical
+        {
+            let x = target.distance(*camera.position());
+            *speed = 0.5 * smoothstep(*min, *max, x) + 0.001;
+        }
+        self.control.handle_events(camera, events)
+    }
+}
+
+fn smoothstep(edge0: f32, edge1: f32, x: f32) -> f32 {
+    let t = ((x - edge0) / (edge1 - edge0)).max(0.0).min(1.0);
+    t * t * (3.0 - 2.0 * t)
+}
--- app-egui-three-d/src/main.rs
+++ app-egui-three-d/src/main.rs
@@ -1,3 +1,6 @@
+pub mod camera_orbit_control;
+
+use camera_orbit_control::CameraOrbitControl;
 use three_d::*;
 
 pub fn main() {
@@ -15,13 +18,19 @@ pub fn main() {
     // Create a camera
     let mut camera = Camera::new_perspective(
         window.viewport(),
-        vec3(0.0, 0.0, 2.0),
-        vec3(0.0, 0.0, 0.0),
+        // vec3(0.0, 0.0, 2.0),
+        // vec3(0.0, 0.0, 0.0),
+        // vec3(0.0, 1.0, 0.0),
+        vec3(0.5, 3.6, 2.6),
+        vec3(0.0, 0.0, -0.8),
         vec3(0.0, 1.0, 0.0),
         degrees(45.0),
         0.1,
         10.0,
     );
+    let mut orbit_drag_speed = 0.1;
+    let mut control = CameraOrbitControl::new(*camera.target(), 0.1, 100.0);
+    control.set_orbit_drag_speed(orbit_drag_speed);
 
     // Create a CPU-side mesh consisting of a single colored triangle
     let positions = vec![
@@ -42,11 +51,13 @@ pub fn main() {
 
     // Construct a model, with a default color material, thereby transferring the mesh data to the GPU
     let mut model = Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
+    let mut axes = Axes::new(&context, 0.05, 0.8);
 
     // Start the main render loop
     let mut gui = three_d::GUI::new(&context);
     let mut angle = 0.0;
-    window.render_loop(move |mut frame_input: FrameInput| // Begin a new frame with an updated frame input
+    window.render_loop(
+        move |mut frame_input: FrameInput| // Begin a new frame with an updated frame input
     {
 
         gui.update(
@@ -58,6 +69,10 @@ pub fn main() {
                 use three_d::egui::*;
                 Window::new("window").show(gui_context, |ui| {
                     ui.add(Slider::new(&mut angle, 0.01..=10.0).text("angle"));
+
+                    if ui.add(Slider::new(&mut orbit_drag_speed, 0.01..=0.5).text("orbit drag speed")).changed() {
+                        control.set_orbit_drag_speed(orbit_drag_speed);
+                    }
                 });
                 // SidePanel::left("side_panel").show(gui_context, |ui| {
                 //     use three_d::egui::*;
@@ -69,9 +84,11 @@ pub fn main() {
             },
         );
         model.set_transformation(Mat4::from_angle_y(radians((angle) as f32)));
+        axes.set_transformation(Mat4::from_angle_y(radians((angle) as f32)));
 
         // Ensure the viewport matches the current window viewport which changes if the window is resized
         camera.set_viewport(frame_input.viewport);
+        control.handle_events(&mut camera, &mut frame_input.events);
 
         // // Set the current transformation of the triangle
         // model.set_transformation(Mat4::from_angle_y(radians((frame_input.accumulated_time * 0.005) as f32)));
@@ -84,9 +101,13 @@ pub fn main() {
             .render(
                 &camera, &[&model], &[]
             )
+            .render(
+                &camera, &[&axes], &[]
+            )
             .write(|| gui.render());
 
         // Returns default frame output to end the frame
         FrameOutput::default()
-    });
+    },
+    );
 }
